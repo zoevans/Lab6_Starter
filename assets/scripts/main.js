57795c8f53fba7aebaa948d2705517569b62fae7
Zoey Evans <zoevans@ucsd.edu>
2025-05-12 23:12:09 -0700

    Completed A9 in main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,9 @@ function getRecipesFromStorage() {
 	// A9. TODO - Complete the functionality as described in this function
 	//           header. It is possible in only a single line, but should
 	//           be no more than a few lines.
+	const recipes = localStorage.getItem('recipes');
+	if(!recipes){ return []; }
+	return JSON.parse(recipes);
 }
 
 /**
@@ -35,10 +38,14 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
 	// A10. TODO - Get a reference to the <main> element
+
+
 	// A11. TODO - Loop through each of the recipes in the passed in array,
 	//            create a <recipe-card> element for each one, and populate
 	//            each <recipe-card> with that recipe data using element.data = ...
 	//            Append each element to <main>
+	
+
 }
 
 /**
