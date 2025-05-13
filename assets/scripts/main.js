90e454ab5ed8d219f7d80dfde788e7955a5932fc
Zoey Evans <zoevans@ucsd.edu>
2025-05-13 00:25:43 -0700

    Started Explore up to and including B9 in main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -62,6 +62,7 @@ function saveRecipesToStorage(recipes) {
 	// B1. TODO - Complete the functionality as described in this function
 	//            header. It is possible in only a single line, but should
 	//            be no more than a few lines.
+	localStorage.setItem('recipes', JSON.stringify(recipes));
 }
 
 /**
@@ -70,18 +71,43 @@ function saveRecipesToStorage(recipes) {
  */
 function initFormHandler() {
 	// B2. TODO - Get a reference to the <form> element
+	let form = document.querySelector('form');
 	// B3. TODO - Add an event listener for the 'submit' event, which fires when the
 	//            submit button is clicked
 	// Steps B4-B9 will occur inside the event listener from step B3
+	form.addEventListener('submit', (event) => {
+	
 	// B4. TODO - Create a new FormData object from the <form> element reference above
+	let formData = new FormData(form);
+
 	// B5. TODO - Create an empty object (we'll refer to this object as recipeObject to
 	//            make this easier to read), and then extract the keys and corresponding
 	//            values from the FormData object and insert them into recipeObject
+	let recipeObject = {};
+	for(let [key, value] of formData.entries()){
+		recipeObject[key] = value;
+	}
+
 	// B6. TODO - Create a new <recipe-card> element
+	let newRecipeCard = document.createElement('recipe-card');
+
 	// B7. TODO - Add the recipeObject data to <recipe-card> using element.data
+	newRecipeCard.data = recipeObject;
+
 	// B8. TODO - Append this new <recipe-card> to <main>
+	let main = document.querySelector('main');
+	main.appendChild(newRecipeCard);
+
 	// B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
 	//            then save the recipes array back to localStorage
+	let recipes = getRecipesFromStorage();
+	recipes.push(recipeObject);
+	saveRecipesToStorage(recipes);
+
+
+	});
+	
+
 	// B10. TODO - Get a reference to the "Clear Local Storage" button
 	// B11. TODO - Add a click event listener to clear local storage button
 	// Steps B12 & B13 will occur inside the event listener from step B11
